0709fdb7406278cf9a85413a841ba4e3c61f2813
Valentina <valentina@Valentinas-Air.frontierlocal.net>
2017-11-19 15:39:37 -0800

    hiding navigation bar using jquery

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,4 +7,16 @@ $(document).ready(function(){
   pauseOnFocus: false,
   pauseOnHover: false
   });
+  var scroller = 0;
+  $(window).scroll(function(){
+    var topScroll = $(this).scrollTop();
+    if (topScroll - scroller >= 50) {
+      var heightNavBar = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + heightNavBar}, 150);
+      scroller = topScroll;
+    } else if (scroller - topScroll >= 50){
+      $('.navbar').animate({top: '0px'}, 150);
+      scroller = topScroll;
+    }
+  });
 });
